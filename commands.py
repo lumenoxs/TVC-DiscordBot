79373ff1af1c28caaa52cfffb1bee45bf631fd5e
lumenoxs <190932586+lumenoxs@users.noreply.github.com>
2025-09-03 15:53:56 +0100

    feat: enhance moderation functionality with message tracking and warning system

--- commands.py
+++ commands.py
@@ -38,81 +38,17 @@ class CommandsCog(commands.Cog):
             color=discord.Color.blue()
         )
         await ctx.send(embed=main)
-        
-    # Server info
-
-    @commands.command()
-    async def info(self, ctx):
-        if ctx.channel.id != INFO["CHANNELS"]["BOT"]:
-            try:
-                await ctx.message.delete()
-            except discord.Forbidden:
-                pass
-            await ctx.send(f"Please use all commands in <#{INFO["CHANNELS"]["BOT"]}>.", delete_after=5)
-            return
-        url = f"https://api.mcstatus.io/v2/status/java/{INFO["IP"]}"
-        data = get_data(url)
-        
-        server_status = "**Status:** `Offline`" if not data or not data["online"] else "**Status:** `Online`"
-        
-        embed = discord.Embed(
-            title="Server Info",
-            description=f"**IP:** `{INFO["IP"]}`\n**Version:** `{INFO["VERSIONS"]}`\n{server_status}",
-            color=discord.Color.blue()
-        )
-        
-        await ctx.send(embed=embed)
-        
-    # Vote links
-
-    @commands.command()
-    async def vote(self, ctx):
-        main = discord.Embed(
-            title="Vote",
-            description="**Thank you for voting!**\n[1 - VOTE](https://example.com)",
-            color=discord.Color.blue()
-        )
-        await ctx.send(embed=main)
-
-    # Server status
-
-    @commands.command()
-    async def status(self, ctx):
-        if ctx.channel.id != INFO["CHANNELS"]["BOT"]:
-            try:
-                await ctx.message.delete()
-            except discord.Forbidden:
-                pass
-            await ctx.send(f"Please use all commands in <#{INFO["CHANNELS"]["BOT"]}>.", delete_after=5)
-            return
-        url = f"https://api.mcstatus.io/v2/status/java/{INFO["IP"]}"
-        data = get_data(url)
-        
-        if data and data["online"]:
-            embed = discord.Embed(
-                title="Server is online",
-                description="",
-                color=discord.Color.green()
-            )
-        else:
-            embed = discord.Embed(
-                title="Server is offline",
-                description="",
-                color=discord.Color.red()
-            )
-
-        await ctx.send(embed=embed)
-
+    
     # Discord member count
 
     @commands.command()
     async def members(self, ctx):
-        if ctx.channel.id != INFO["CHANNELS"]["BOT"]:
+        if ctx.channel.id != channels["bot"]:
             try:
                 await ctx.message.delete()
             except discord.Forbidden:
                 pass
-            await ctx.send(f"Please use all commands in <#{INFO["CHANNELS"]["BOT"]}>.", delete_after=5)
+            await ctx.send(f"Please use all commands in <#{channels["bot"]}>.", delete_after=3)
             return
         member_count = ctx.guild.member_count
         embed = discord.Embed(
@@ -122,10 +58,10 @@ class CommandsCog(commands.Cog):
         )
         await ctx.send(embed=embed)
 
-    @commands.event
+    @commands.Cog.listener()
     async def on_command_error(ctx, error):
         if isinstance(error, discord.ext.commands.errors.CommandNotFound):
-            await ctx.send("Not a command >:|")
+            await ctx.send("Couldnt find that command :/")
             print(f"Wrong command ran by {ctx.author.display_name}, {ctx.author.name}")
         else:
             raise error
--- moderation.py
+++ moderation.py
@@ -31,6 +31,9 @@ def ordinal(n):
     return "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
 
 class ModerationCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def warn(self, ctx, *args):
@@ -38,7 +41,37 @@ class ModerationCog(commands.Cog):
         reason = " ".join(args[1:]) if len(args) > 1 else "No reason provided"
         save_warns_data(user.id, reason)
         await ctx.send(f"Warned <@{user.id}> for reason: {reason}\nThis is their {ordinal(number_warns(user.id))} strike.")
+    
+    @commands.Cog.listener()
+    async def on_raw_message_delete(self, plyd):
+        if plyd.message.author.bot or plyd.cached_message:
+            return
+        channel = self.bot.get_channel(1312528601253412945)
+        dchannel = self.bot.get_channel(plyd.channel_id)
+        await channel.send(f"Message deleted in {dchannel.mention}")
         
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        if message.author.bot:
+            return
+        channel = self.bot.get_channel(1312528601253412945)
+        await channel.send(f"Message by {message.author.name} deleted in {message.channel.mention}:\n```{message.content}```")
+    
+    @commands.Cog.listener()
+    async def on_raw_message_edit(self, plyd):
+        if plyd.message.author.bot or plyd.cached_message:
+            return
+        channel = self.bot.get_channel(1312528601253412945)
+        await channel.send(f"Message by {plyd.message.author.name} edited in {plyd.message.channel.mention}\nMessage:\n```{plyd.message.content}```")
+    
+    @commands.Cog.listener()
+    async def on_message_edit(self, message_before, message_after):
+        if message_before.author.bot:
+            return
+        channel = self.bot.get_channel(1312528601253412945)
+        await channel.send(f"Message by {message_after.author.name} edited in {message_after.channel.mention}\nOld message:\n```{message_before.content}```\nNew message:\n```{message_after.content}```")
+    
+    
     # @commands.command()
     # async def purge(self, ctx, number: int):
     #     if admin(ctx.author):

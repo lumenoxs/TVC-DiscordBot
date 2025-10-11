d48880e5660950ee3d322a14594effb93c6c23da
lumenoxs <190932586+lumenoxs@users.noreply.github.com>
2025-10-11 18:34:04 +0100

    added back custom emojis for roles messae, and changed the command names and added the command roles_add_reactions()

--- roles.py
+++ roles.py
@@ -7,6 +7,10 @@ roles["📊"] = 1313604458143420526
 roles["🚨"] = 1313604650372563016
 roles["🍎"] = 1412722373777428500
 roles["🍃"] = 1412722480593899622
+roles[1355953585543713059] = 1424466785431716031 # 1.8
+roles[1426619624602079342] = 1424463163805536366 # 1.9+
+roles[1426620072914325625] = 1424463049393442989 # mace
+roles[1426620730002641116] = 1426621910070067210 # pots
 
 roles_file = "rolesmsg.txt"
 
@@ -20,14 +24,23 @@ class RolesCog(commands.Cog):
         if user.bot:
             return
         message = await self.bot.get_channel(plyd.channel_id).fetch_message(plyd.message_id)
-        if message.channel.id == 1412708336536653886 and not message.id == 0 and plyd.emoji.name in roles.keys():
-            role = user.guild.get_role(roles[plyd.emoji.name])
-            if role in user.roles:
-                await user.send(f"You already have the role {role.name}!")
-                return
-            print(f"Added role {role.name} to {user.display_name}")
-            await user.add_roles(role)
-            await user.send(f"Added role {role.name}!")
+        if message.channel.id == 1412708336536653886 and not message.id == 0:
+            if plyd.emoji.name in roles.keys():
+                role = user.guild.get_role(roles[plyd.emoji.name])
+                if role in user.roles:
+                    await user.send(f"You already have the role {role.name}!")
+                    return
+                print(f"Added role {role.name} to {user.display_name}")
+                await user.add_roles(role)
+                await user.send(f"Added role {role.name}!")
+            elif plyd.emoji.id in roles.keys():
+                role = user.guild.get_role(roles[plyd.emoji.id])
+                if role in user.roles:
+                    await user.send(f"You already have the role {role.name}!")
+                    return
+                print(f"Added role {role.name} to {user.display_name}")
+                await user.add_roles(role)
+                await user.send(f"Added role {role.name}!")
                     
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, plyd):
@@ -35,18 +48,28 @@ class RolesCog(commands.Cog):
         if user.bot:
             return
         message = await self.bot.get_channel(plyd.channel_id).fetch_message(plyd.message_id)
-        if message.channel.id == 1412708336536653886 and not message.id == 0 and plyd.emoji.name in roles.keys():
-            role = user.guild.get_role(roles[plyd.emoji.name])
-            if role not in user.roles:
-                await user.send(f"You don't have the role {role.name}!")
-                return
-            print(f"Removed role {role.name} from {user.display_name}")
-            await user.remove_roles(role)
-            await user.send(f"Removed role {role.name}!")
+        if message.channel.id == 1412708336536653886 and not message.id == 0:
+            if plyd.emoji.name in roles.keys():
+                role = user.guild.get_role(roles[plyd.emoji.name])
+                if role not in user.roles:
+                    await user.send(f"You don't have the role {role.name}!")
+                    return
+                print(f"Removed role {role.name} from {user.display_name}")
+                await user.remove_roles(role)
+                await user.send(f"Removed role {role.name}!")
+            elif plyd.emoji.id in roles.keys():
+                role = user.guild.get_role(roles[plyd.emoji.id])
+                if role not in user.roles:
+                    await user.send(f"You don't have the role {role.name}!")
+                    return
+                print(f"Removed role {role.name} from {user.display_name}")
+                await user.remove_roles(role)
+                await user.send(f"Removed role {role.name}!")
     
     @commands.command()
-    async def updaterolesmsg(self, ctx):
+    async def roles_message(self, ctx):
         if ctx.author.guild_permissions.administrator == False: return
+
         with open(roles_file) as file:
             content = file.read()
 
@@ -54,7 +77,7 @@ class RolesCog(commands.Cog):
         await msg.edit(content=content)
 
     @commands.command()
-    async def updaterolesreactions(self, ctx):
+    async def roles_reactions(self, ctx):
         if ctx.author.guild_permissions.administrator == False: return
         
         msg = await self.bot.get_channel(1412708336536653886).fetch_message(1413137650063511674)
@@ -70,6 +93,16 @@ class RolesCog(commands.Cog):
                     print(f"Added role {role.name} to {member.display_name}")
                     await member.add_roles(role)
                     await member.send(f"The bot was down when you reacted to the message. Added role {role.name}!")
+
+    @commands.command()
+    async def roles_add_reactions(self, ctx):
+        if ctx.author.guild_permissions.administrator == False: return
+        
+        msg = await self.bot.get_channel(1412708336536653886).fetch_message(1413137650063511674)
+        for emoji in roles.keys():
+            if type(emoji) == int:
+                emoji = self.bot.get_emoji(emoji)
+            await msg.add_reaction(emoji)
             
 async def setup(bot):
     await bot.add_cog(RolesCog(bot))
